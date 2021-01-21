33d70f6a30ea8bc9416a96b1c9fe5f60a4d481c2
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-21 12:26:39 -0500

    Added seed data for widgets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react' 
 import Accordion from './components/Accordion'
 
+const items = [
+    {
+        title: 'What is React?',
+        content: "React is a front end JavaScript framework"
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components.'
+    }
+]
 
 export default () => {
     return (
