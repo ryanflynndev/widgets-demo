a3f482826338d0555bac16fec5ebcf089bc99b0e
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-21 21:37:13 -0500

    Added accordion functionality

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,19 +1,25 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 
 const Accordion = ({ items }) => {
+
+    const [activeIndex, setActiveIndex] = useState(null)
+
     const clickHandler = (index) => {
-        console.log(index)
+        setActiveIndex(index)
     }
     const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : ''
+
         return (
         <React.Fragment key={item.title}>
             <div 
-            className="title active" 
+            className={`title ${active}`} 
             onClick={() => clickHandler(index)}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
         </React.Fragment>
