63b103a7603daff8edbe5cf9f4d2dbf022566688
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-22 12:24:29 -0500

    Finished useEffect  search

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import React from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 
-const wiki = [
-    {
-        title: 'Computer Programming',
-        pageId: 5311,
-        snippet: 'bla blaaa bla bla bla bla blaaa'
-    },
-    {
-        title: 'Program',
-        pageId: 23771,
-        snippet: 'bla blaaa bla bla bla bla blaaa'
-    },
-    {
-        title: 'Programming Language',
-        pageId: 23015,
-        snippet: 'bla bla bla'
-    }
-]
+
 
 const items = [
     {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Search = () => {
-const [searchTerm, setSearchTerm] = useState('')
-
+    const [searchTerm, setSearchTerm] = useState('')
+    const wiki = [
+        {
+            title: 'Computer Programming',
+            pageId: 5311,
+            snippet: 'bla blaaa bla bla bla bla blaaa'
+        },
+        {
+            title: 'Program',
+            pageId: 23771,
+            snippet: 'bla blaaa bla bla bla bla blaaa'
+        },
+        {
+            title: 'Programming Language',
+            pageId: 23015,
+            snippet: 'bla bla bla'
+        }
+    ]
+    useEffect(() => {
+        const array = wiki.filter((item) => {
+            item.title[0].includes(searchTerm[0])
+        })
+        console.log(array)
+    }, [searchTerm])
     return(
         <div className = "ui form">
             <div className="field">
