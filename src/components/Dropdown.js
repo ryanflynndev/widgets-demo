9130ec4bcc29ab016bb61edb5a5b2d322306add8
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-23 11:15:27 -0500

    Filtered dropdown so it does not list the color that is selected

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const renderedOptions = options.map((item) => {
+        if(item.value === selected.value){
+            return null
+        }
         return (
             <div key={item.value} className="item" onClick={() => onSelectedChange(item)}>{item.label}</div>
         )
