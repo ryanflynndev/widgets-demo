62c296969becb5a8191f9991684fe00d3a529130
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-25 09:05:35 -0500

    Created body event listener so dropdown will exit if you clcik somewhere else

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react' 
+import React, { useState, useEffect } from 'react' 
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
     const [open, setOpen] = useState(false)
-
+    useEffect(() => {{
+        document.body.addEventListener('click', () => {
+            setOpen(false)
+        })
+    }}, [])
     const renderedOptions = options.map((item) => {
         if(item.value === selected.value){
             return null
