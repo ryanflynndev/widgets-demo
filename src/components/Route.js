9ffe05412a3cbb34ce63c04879507eb04474ba1e
Ryan Flynn <ryanflynn44@gmail.com>
2021-02-17 19:17:07 -0500

    Built out logic for route component

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,7 +1,9 @@
 import React from 'react' 
 
-const Route = () => {
-
+const Route = ( { path, children }) => {
+    return window.location.pathname === path
+    ? children 
+    : null
 }
 
 export default Route;
